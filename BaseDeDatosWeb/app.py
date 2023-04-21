3da2301756ad63c90e932a47cabf668f0cd02af6
LK4SNIPER <131406228+LK4SNIPER@users.noreply.github.com>
2023-04-21 18:09:29 -0500

    Update app.py

--- BaseDeDatosWeb/app.py
+++ BaseDeDatosWeb/app.py
@@ -6,22 +6,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/Graph1', methods=['GET'])
+@app.route('/graph1')
 def graph1():
-    return'Test site'
+    # Lógica para Graph1
+    return 'Esta es la ruta para Graph1'
 
-@app.route('/Graph2')
+@app.route('/graph2')
 def graph2():
     # Lógica para Graph2
     return 'Esta es la ruta para Graph2'
 
-@app.route('/Graph3')
+@app.route('/graph3')
 def graph3():
     # Lógica para Graph3
     return 'Esta es la ruta para Graph3'
 
-@app.route('/Graph4')
-def graph4():
+@app.route('/graph4')
+def graph3():
     # Lógica para Graph4
     return 'Esta es la ruta para Graph4'
 
